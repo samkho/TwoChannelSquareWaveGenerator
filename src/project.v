d2bf8d8f2b3919f917c2b1a024b1ebb50cf21d44
samkho <215086+samkho@users.noreply.github.com>
2024-11-10 05:03:04 -0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -12,7 +12,7 @@
 */
 `default_nettype none
 
-module tt_um_example (
+module tt_um_samkho_two_channel_square_wave_generator (
   input  wire [7:0] ui_in,    // Dedicated inputs
   output wire [7:0] uo_out,   // Dedicated outputs
   input  wire [7:0] uio_in,   // IOs: Input path
